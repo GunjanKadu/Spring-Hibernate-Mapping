b6460e9fcc2c8923bc5503d118b99636ac5828b4
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-05 01:11:43 +0200

    GetInstructor from instrutor details main class

---
+++ hb-02-one-to-one-bi/src/com/gk/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,45 @@
+package com.gk.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.gk.hibernate.demo.entity.Instructor;
+import com.gk.hibernate.demo.entity.InstructorDetails;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetails.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// get the instructor details object
+			int theId = 3;
+			InstructorDetails theInstructorDetails = session.get(InstructorDetails.class, theId);
+
+			// print the instructor detail
+			System.out.println("The Instructor Details found is " + theInstructorDetails);
+
+			// print the associated instructor
+			System.out.println("The Associated Instructor is " + theInstructorDetails.getInstructor());
+
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done");
+
+		} finally {
+			// TODO: handle finally clause
+		}
+
+	}
+
+}

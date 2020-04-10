6d33a237280e2d6631dc4073cb4457a3aeefc48d
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-10 20:40:24 +0200

    Deleting the course

---
+++ hb-04-one-to-many-uni/src/com/gk/hibernate/demo/DeleteCoursesAndReviewsDemo.java
@@ -0,0 +1,53 @@
+package com.gk.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.gk.hibernate.demo.entity.Course;
+import com.gk.hibernate.demo.entity.Instructor;
+import com.gk.hibernate.demo.entity.InstructorDetails;
+import com.gk.hibernate.demo.entity.Review;
+
+public class DeleteCoursesAndReviewsDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetails.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// get a course
+			int theId = 10;
+			Course tempCourse = session.get(Course.class, theId);
+
+			// print the course
+			System.out.println("Deleting the course");
+			System.out.println(tempCourse);
+
+			// print the course reviews
+			System.out.println(tempCourse.getReviews());
+
+			// delete the course
+			session.delete(tempCourse);
+
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done!!");
+
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}

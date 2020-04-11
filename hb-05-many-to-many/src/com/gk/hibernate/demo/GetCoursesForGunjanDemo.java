2208901584c69ad20182e9bfb1f7e626d4c3249c
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-12 00:43:17 +0200

    Get courses for a studetn to verify

---
+++ hb-05-many-to-many/src/com/gk/hibernate/demo/GetCoursesForGunjanDemo.java
@@ -0,0 +1,47 @@
+package com.gk.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.gk.hibernate.demo.entity.Course;
+import com.gk.hibernate.demo.entity.Instructor;
+import com.gk.hibernate.demo.entity.InstructorDetails;
+import com.gk.hibernate.demo.entity.Review;
+import com.gk.hibernate.demo.entity.Student;
+
+public class GetCoursesForGunjanDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetails.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class).addAnnotatedClass(Student.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			int theId = 1;
+
+			// get the student gunjan from the database
+			Student tempStudent = session.get(Student.class, theId);
+			System.out.println("Loaded Student" + tempStudent);
+			System.out.println("Courses: " + tempStudent.getCourses());
+
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done!!");
+
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
